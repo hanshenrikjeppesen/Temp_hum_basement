4bb064a0d443f3b1d47eb3fab12f81d4771e416a
hanshenrikjeppesen <info@frist.dk>
2018-01-29 22:27:49 +0100

    Create read_sensor.py

---
+++ read_sensor.py
@@ -0,0 +1,20 @@
+import os
+from time import sleep
+os.chdir('DHT22_py')
+import pigpio
+
+pi = pigpio.pi()
+
+import DHT22.sensor(pi, 4)
+
+temp = 0
+hum = 0
+
+for i in range(10):
+    sleep(2)
+    s.trigger()
+    temp = temp + str(s.temperature())
+
+temp = temp/10
+print('avag temp is=: ' + temp)
+
