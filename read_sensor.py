c9f9f5a37f3b874873f7b8880a39046e88837590
hanshenrikjeppesen <info@frist.dk>
2018-01-29 22:44:02 +0100

    Update read_sensor.py

--- read_sensor.py
+++ read_sensor.py
@@ -10,6 +10,7 @@ s = DHT22.sensor(pi, 4)
 temp = 0
 hum = 0
 
+'''
 for i in range(10):
     sleep(2)
     s.trigger()
@@ -17,4 +18,9 @@ for i in range(10):
 
 temp = temp/10
 print('avag temp is=: ' + str(temp))
+'''
+s.trigger()
+print('Temperature is {:3.2f}'.format(s.temperature() / 1.))
+print('Humidity is {:3.2f}'.format(s.humidity() / 1.))
 
+      
