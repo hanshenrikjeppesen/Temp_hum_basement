e1aebb86a7ce719348e4dfe0024c98af6460124e
hanshenrikjeppesen <info@frist.dk>
2018-01-29 22:54:48 +0100

    Update read_sensor.py

--- read_sensor.py
+++ read_sensor.py
@@ -1,12 +1,11 @@
 from time import sleep
 import pigpio
-# sleep(2)
+
 pi = pigpio.pi()
-# sleep(1)
+
 import DHT22
-# sleep(1)
 s = DHT22.sensor(pi, 4)
-# sleep(1)
+
 s.trigger()
 sleep(1)
 print(s.temperature())
