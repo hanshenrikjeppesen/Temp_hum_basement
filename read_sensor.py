bb55b70ee43acbe4b4f7310c91cef50ac122105c
hanshenrikjeppesen <info@frist.dk>
2018-02-03 21:16:43 +0100

    Update read_sensor.py

--- read_sensor.py
+++ read_sensor.py
@@ -8,7 +8,11 @@ s = DHT22.sensor(pi, 4)
 
 s.trigger()
 sleep(1)
-print('Temperature is {:.1f}'.format(s.temperature()))
-print('Humidity is {:.1f}'.format(s.humidity()))
+# print('Temperature is {:.1f}'.format(s.temperature()))
+# print('Humidity is {:.1f}'.format(s.humidity()))
+temp = s.temperature()
+hum = s.humidity()
 
+print(temp)
+print(hum)
       
