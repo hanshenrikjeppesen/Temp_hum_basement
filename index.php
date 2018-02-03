46f58568f04de0d12e8dbfae209a147b4de59b6f
hanshenrikjeppesen <info@frist.dk>
2018-02-03 23:14:28 +0100

    Update index.php

--- index.php
+++ index.php
@@ -7,6 +7,10 @@
 	<h1>Project: Monitoring data in my home basement</h1>
 	<?php
 		$myfile =  fopen("/home/hans/temp_log_file.txt", "r") or die("can't finde the file on server");
-		echo fread($myfile,filesize("/home/hans/temp_log_file.txt"));
+		while(! feof($myfile))
+  			{
+  			echo fgets($myfile). "<br />";
+  			}
+		/* echo fread($myfile,filesize("/home/hans/temp_log_file.txt")); */
 		fclose($myfile);
 		?>
