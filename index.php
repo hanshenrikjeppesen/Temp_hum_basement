09f730d4102dc7e4c09c9c27da49751cef77fdd5
hanshenrikjeppesen <info@frist.dk>
2018-02-03 22:05:27 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>RaspberryPi - Basement monitoring</title>
+</head>
+<body>
+	<h1>Project: Monitoring data in my home basement</h1>
+	<?php
+		$myfile =  fopen("/home/hans/temp_log_file.txt", "r") or die("can't finde the file on server");
+		echo fread($myfile,filesize("/home/hans/temp_log_file.txt"));
+		fclose($myfile);
+		?>
