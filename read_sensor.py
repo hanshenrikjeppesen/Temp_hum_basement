7d42a62580ce325e69e2442bb09d88e9340fd9c5
hanshenrikjeppesen <info@frist.dk>
2018-02-03 21:33:26 +0100

    Update read_sensor.py

--- read_sensor.py
+++ read_sensor.py
@@ -1,6 +1,7 @@
 from time import sleep
 import pigpio
 
+path_to_file = "/home/hans/temp_log_file"
 pi = pigpio.pi()
 
 import DHT22
@@ -10,9 +11,10 @@ s.trigger()
 sleep(1)
 # print('Temperature is {:.1f}'.format(s.temperature()))
 # print('Humidity is {:.1f}'.format(s.humidity()))
-temp = s.temperature()
-hum = s.humidity()
-
+temp = ('Temperature is {:.1f}'.format(s.temperature()))
+hum = ('Humidity is {:.1f}'.format(s.humidity()))
 print(temp)
-print(hum)
+with open(path_to_file, 'a') as f:
+      f.write(temp)
+      
       
